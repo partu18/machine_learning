ce0f0e80afb076c036ca057c159640b955852b51
Laouen <laouen.belloli@gmail.com>
2016-09-10 20:39:04 -0300

    terminadas las funciones para extraer las features de ngrams en ngrams_features

--- trabajos_practicos/tp1/src/ngram_features.py
+++ trabajos_practicos/tp1/src/ngram_features.py
@@ -1,1 +1,59 @@
+from constants import EMAIL_TEXT
 import ngrams as ng
+import json
+
+def get_1grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=1, separator=separator)
+    features = json.load(open('ngram_features/1grams.json'))
+    for ft in features:
+       features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_2grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=2, separator=separator)
+    features = json.load(open('ngram_features/2grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_3grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=3, separator=separator)
+    features = json.load(open('ngram_features/3grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_4grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=4, separator=separator)
+    features = json.load(open('ngram_features/4grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_5grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=5, separator=separator)
+    features = json.load(open('ngram_features/5grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_7grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=7, separator=separator)
+    features = json.load(open('ngram_features/7grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
+
+def get_10grams_features(email, separator='partugabylao'):
+    features_count = dict()
+    email_ngrams = ng.find_ngrams(email_structure[EMAIL_TEXT], n=10, separator=separator)
+    features = json.load(open('ngram_features/10grams.json'))
+    for ft in features:
+        features_count[ft] = email_ngrams.count(ft)
+    return features_count
