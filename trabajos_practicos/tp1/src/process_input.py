2aed1de712581c1eac31dcb68fe99441adc3d5c5
Laouen <laouen.belloli@gmail.com>
2016-09-17 02:32:24 -0300

    emprolijado run_classifier para poder corer grids tomando como parametros, el dataset X, las clases y los parametros en json deseados.

--- trabajos_practicos/tp1/src/process_input.py
+++ trabajos_practicos/tp1/src/process_input.py
@@ -1,5 +1,4 @@
-#import ngram_features
-import mime_headers_features
+# general purpose
 from random import randint
 import pandas as pd
 import numpy as np
@@ -13,53 +12,26 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.decomposition import TruncatedSVD
 from sklearn.grid_search import GridSearchCV
 
+# features
+import features/mime_headers_features
+import features/single_features
+
 # Text processing
 from inspect import getmembers, isfunction
 from emailInfoExtractor import *
 from helper import Helper
-import features
 
-
-features_functions = [m for m in getmembers(features) if isfunction(m[1])]
-#ngram_features_functions = [m for m in getmembers(ngram_features) if isfunction(m[1])]
 mime_headers_features_functions = [m for m in getmembers(mime_headers_features) if isfunction(m[1])]
-
-def local_grid_search(X,y,curr_depth,curr_features,feature_amount,neighbor_depth=1):
-    local_params = dict()
-    local_params['splitter'] = ['best','random']
-    local_params['max_features'] = range(max(curr_features-neighbor_depth,1),min(curr_features+neighbor_depth+1,feature_amount))
-    local_params['max_depth'] = range(max(curr_depth-neighbor_depth,1),curr_depth+neighbor_depth+1)
-    return grid.fit(X,y)
-
-
-def grid_search(X,y,clasificators,params_grids):
-    
-    for clf in clasificators:
-        grid = GridSearchCV(clf, local_params, cv=10)
-    grid = local_grid_search(X,y,curr_depth,curr_features,feature_amount)
-    print grid.best_score_, last_score
-    print grid.best_params_
-
-    ## start local search
-    while grid.best_score_ > last_score:
-        last_score = grid.best_score_
-        grid = local_grid_search(X,y,grid.best_params_['max_depth'],grid.best_params_['max_features'],feature_amount)
-        print grid.best_score_, last_score
-        print grid.best_params_
-
-    return grid
+features_functions = [m for m in getmembers(features) if isfunction(m[1])]
 
 def preprocess(raw_emails):
     return [get_email_info_structure(mail) for mail in raw_emails]
 
 def process_email(email):
-    #simple features
+    # Simple features
     final_features = {name:extractor(email) for (name,extractor) in features_functions}
-    #for (name, extractor) in ngram_features_functions:
-    #    ng_features = extractor(email)
-    #    for feature_name,value in ng_features.iteritems():
-    #        final_features[feature_name] = value
 
+    # Mime features
     for (name, extractor) in mime_headers_features_functions:
         header_features = extractor(email)
         for feature_name,value in header_features.iteritems():
@@ -67,8 +39,7 @@ def process_email(email):
 
     return final_features
 
-if __name__ == "__main__":
-
+def features_extraction():
     helper = Helper()
 
     print "Leyendo jsons"
@@ -79,38 +50,14 @@ if __name__ == "__main__":
     print "Preprocesando hams"
     preprocessed_hams = preprocess(ham_emails)
 
-    # Extraigo atributos
     print "Extrayendo features de spam"
     processed_spams = [process_email(mail) for mail in preprocessed_spams] # Multiprocessing?
     print "Extrayendo features de ham"
     processed_hams = [process_email(mail) for mail in preprocessed_hams] # Multiprocessing?
 
-    print "Contruyendo el vector con las clases"
-    y = ['ham' for _ in range(len(ham_emails))]+['spam' for _ in range(len(spam_emails))]
+    print "Extrayendo textos para ngrams de spam"
+    text_hams = [email[EMAIL_TEXT] for email in preprocessed_hams]
+    print "Extrayendo textos para ngrams de ham"
+    text_spams = [email[EMAIL_TEXT] for email in preprocessed_spams]
 
-    print "Creando dataframe.."
-    vectorizer = TfidfVectorizer(ngram_range=(1,3),stop_words='english',max_features=5000)
-    email_texts = [email[EMAIL_TEXT] for email in preprocessed_hams+preprocessed_spams]
-    X_train = vectorizer.fit_transform(email_texts)
-    svd = TruncatedSVD(n_components=100, random_state=42)
-    transformed_x_train = svd.fit_transform(X_train)
-    X = pd.DataFrame(transformed_x_train)
-
-    extracted_features = processed_hams[0].keys()
-    for extracted_feature in extracted_features:  ## ESTE FOR TARDA MUCHO!!!!!
-        print "Escribiendo feature " + extracted_feature
-        X[extracted_feature] = [mail[extracted_feature] for mail in (processed_hams+processed_spams)]
-
-    #print haciendo pickle del svd y del dataframe
-    pickle.dump(y,open('y.pickle','w'))
-    pickle.dump(X,open('X.pickle','w'))
-    pickle.dump(svd,open('svd.pickle','wb'))
-
-    # Elijo mi clasificador.
-    clf = RandomForestClassifier()
-
-    # Ejecuto el clasificador entrenando con un esquema de cross validation
-    # de 10 folds.
-    res = cross_val_score(clf, X, y, cv=10, n_jobs=-1)
-    print np.mean(res), np.std(res)
-    # salida: 0.783040309346 0.0068052434174  (o similar)
+    return processed_hams, processed_spams, text_hams, text_spams
--- trabajos_practicos/tp1/src/run_classifier.py
+++ trabajos_practicos/tp1/src/run_classifier.py
@@ -2,15 +2,18 @@
 import numpy as np
 import pandas as pd
 import pickle
+import json
+import sys
 
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+# clasificators
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.grid_search import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 
+from sklearn.cross_validation import cross_val_score
+from sklearn.grid_search import GridSearchCV
 
 ## Posibles scores: http://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter
 # accuracy
@@ -24,44 +27,39 @@ from sklearn.grid_search import GridSearchCV
 # precision
 # recall
 # roc_auc
-def execute_classifier(classifier, X, target, params, n_jobs=1, folds=10, scoring='f1_weighted', grid_search=True, verbose=True): #definir scoring
+def execute_classifier(classifier, X, target, params, n_jobs=1, folds=10, scoring='accuracy', grid_search=True, verbose=True): #definir scoring
     if grid_search:
         grid = GridSearchCV(classifier, params, cv=folds,n_jobs=n_jobs, scoring=scoring, verbose=verbose)
         grid.fit(X,target)
         print "Best Score:", grid.best_score_
         print "Best params:", grid.best_params_
-        return grid.best_params_, grid.best_score_
+        return grid
     else:
         res = cross_val_score(classifier(**params), X, target, cv=folds, n_jobs=n_jobs, scoring=scoring, verbose=verbose)
         print "Mean: ", np.mean(res)
         print "Std: ", np.std(res)
 
+# Recibe 3 parametros: 
+# 1) pickle del dataset a usar (PCA|LSA, features antes|despues)
+# 2) json con la grilla de parametros a usar, mirar la carpeta params
+# 3) nombre del pickle donde guardar el grid con los parametros optimos, lo guarda automaticamente en la carpeta pickles
 
-print "Leyendo dataframe..."
-df = pickle.load(open('df.pickle','r'))
+if __name__ == '__main__':
 
-extracted_features = list(df.columns)
-extracted_features.remove('raw_email')
-extracted_features.remove('class')
+    if len(sys.argv) < 4:
+        print 'usage {script_name} dataset parameters grid_output.pickle'.format(script_name=sys.argv[0])
+        exit(1)
 
-# Preparo data para clasificar
-X = df[extracted_features].values
-y = df['class']
+    clasificators = dict()
+    clasificators['RandomForestClassifier'] = RandomForestClassifier
+    
+    print "Leyendo dataframe con reduccion de dimensionalidad..."
+    X = pickle.load(open('pickles/'+sys.argv[1]+'_X.pisckle','r'))
+    y = pickle.load(open('pickles/'+sys.argv[1]+'y.pickle','r'))
 
-
-### Example for CV
-params = {'max_depth':150}
-print "Comenzando a entrenar..."
-execute_classifier(DecisionTreeClassifier,X,y,params,n_jobs=3, grid_search=False)
-
-## Example for GridCV
-
-###### If want to reduce dataset size for grid_search, do it here uncommenting the code below
-# size = 50000
-# X = df[extracted_features][0:size].values
-# y = df['class'][0:size]
-########
-
-print "Ejecutando Grid Search..."
-params = {'max_features':[50],'max_depth':[10,50,100,150]}
-execute_classifier(DecisionTreeClassifier(),X,y,params,n_jobs=3, grid_search=True)
+    print "Leyendo la grilla de parametros..."
+    params = json.load(open('params/'+sys.argv[2]+'json','r'))
+    
+    print "Ejecutando Grid Search..."
+    grid = execute_classifier(clasificators[sys.argv[3]], X, y, params, n_jobs=-1, grid_search=True)
+    pickle.dump(grid,open('pickles/'+sys.argv[3],'wb'))
