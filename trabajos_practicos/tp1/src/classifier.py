b8f1bdcdd5d85a097d686dc2ff45e4f5805d8cd1
Gabriel Defagot <gabydefa91@gmail.com>
2016-09-26 09:46:14 -0300

    fix orden

--- trabajos_practicos/tp1/src/classifier.py
+++ trabajos_practicos/tp1/src/classifier.py
@@ -13,11 +13,11 @@ from sklearn.metrics import accuracy_score
 # Text processing
 from process_input import *
 
-def add_features(X,hams_features,spams_features):
+def add_features(X,ham_features,spam_features):
     X = pd.DataFrame(X.todense())
-    extracted_features = hams_features[0].keys()
+    extracted_features = ham_features[0].keys()
     for extracted_feature in extracted_features:
-        X[extracted_feature] = [mail[extracted_feature] for mail in (hams_features+spams_features)]
+        X[extracted_feature] = [mail[extracted_feature] for mail in (ham_features+spam_features)]
     return X
 
 if __name__ == '__main__':
@@ -38,21 +38,21 @@ if __name__ == '__main__':
     classifier = pickle.load(open('pickle/{}.pickle'.format(args.classifier+'_X_'+args.decomposition),'rb'))
 
     "Procesando emails"
-    spams_features, hams_features, spams_text, hams_text = features_extraction()
+    ham_features, spam_features, hams_text, spams_text = features_extraction()
     
     print "Extrayendo ngrams features..."
     X = vectorizer.transform(hams_text+spams_text)
 
     if 'antes' in args.decomposition:
         print "Agregando otras features extraidas..."
-        X = add_features(X,hams_features,spams_features)
+        X = add_features(X,ham_features,spam_features)
 
     print "reduciendo dimensionalidad..."
     X = decomposition.transform(X)
 
     if 'despues' in args.decomposition:
         print "Agregando otras features extraidas..."
-        X = add_features(X,hams_features,spams_features)
+        X = add_features(X,ham_features,spam_features)
     
     print "Extrayendo las clases..."
     y = ['ham' for _ in range(len(hams_text))]+['spam' for _ in range(len(spams_text))]
