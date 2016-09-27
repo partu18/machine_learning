0ce4155c8ddc533a383ef39791de6a0d9daefd47
Laouen <laouen.belloli@gmail.com>
2016-09-27 19:47:37 -0300

    subiendo graficos

--- trabajos_practicos/tp1/src/plot.py
+++ trabajos_practicos/tp1/src/plot.py
@@ -7,11 +7,17 @@ sns.set(style="darkgrid")
 df = pd.read_csv(open('../results/scores.csv'))
 
 palette = {'pca-antes':"#5C3908", 'pca-despues':"#0B0D36", 'svd-antes':"#6F625F", 'svd-despues':"#5A1615"}
-ax = sns.barplot(x='classifier', y='f1', hue='decomposition', data=df, palette=palette)
+ax = sns.barplot(x='classifier', y='recall', hue='decomposition', data=df, palette=palette)
 
-ax.set_ylabel('f1 score')
+ax.set_xlabel('Classifier')
+ax.set_ylabel('Recall-score')
 
 plot_margin = 0.25
+
+for p in ax.patches:
+	x = p.get_x()
+	height = p.get_height()
+	ax.text(x + 0.05, height + 0.1, '%1.2f'%(height), rotation='vertical')
 
 x0, x1, y0, y1 = plt.axis()
 plt.axis((x0 - plot_margin,
@@ -19,9 +25,6 @@ plt.axis((x0 - plot_margin,
           y0,
           y1 + 2*plot_margin))
 
-for p in ax.patches:
-	height = p.get_height()
-	ax.text(p.get_x(), height+3, '%1.2f'%(height/10))
 
-plt.legend(bbox_to_anchor=(1.05, 1), loc=0, borderaxespad=3.)
+plt.legend(loc=1)
 plt.show()
--- trabajos_practicos/tp1/src/plot_times.py
+++ trabajos_practicos/tp1/src/plot_times.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import math
 
 sns.set(style="darkgrid")
 
@@ -8,16 +9,16 @@ df = pd.read_csv(open('../results/times.csv'))
 
 palette = {'pca-antes':"#5C3908", 'pca-despues':"#0B0D36", 'svd-antes':"#6F625F", 'svd-despues':"#5A1615"}
 ax = sns.barplot(x='classifier', y='prediction_time', hue='decomposition', data=df, palette=palette)
+ax.set(yscale="log")
+ax.set_xlabel('Classifier')
+ax.set_ylabel('Prediction time (seconds)')
 
-ax.set_ylabel('clocks')
-
-plot_margin = 0.25
+plot_margin = 0.5
 
 for p in ax.patches:
 	x = p.get_x()
-	p.set_width(10)
 	height = p.get_height()
-	ax.text(x, height+0.1, '%1.2f'%(height/10))
+	ax.text(x + 0.05, 3.5*height, '%1.2f'%(height), rotation='vertical')
 
 x0, x1, y0, y1 = plt.axis()
 plt.axis((x0 - plot_margin,
@@ -25,5 +26,5 @@ plt.axis((x0 - plot_margin,
           y0,
           y1 + 2*plot_margin))
 
-plt.legend(bbox_to_anchor=(1.05, 1), loc=0, borderaxespad=3.)
+plt.legend(loc=1)
 plt.show()
